71bc21bb9655d7e0906a8204b4f3c1d68edda3fb
Shabad Lamba <shabadlamba@gmail.com>
2022-06-03 00:55:44 +0530

    removed Universitites

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@
 import './App.scss';
 import FooterMain from './Components/footer/footerMain';
 import HeaderMain from './Components/header/headerMain';
-import SearchBar from './Components/main/searchBar/searchBar';
+import Jokes from './Components/main/jokes/jokes';
+// import SearchBar from './Components/main/searchBar/searchBar';
 
 function App() {
   return (
     <div className='App'>
       <HeaderMain></HeaderMain>
-      <SearchBar appName='Universities'></SearchBar>
+      <Jokes></Jokes>
+      {/* <SearchBar appName='Universities'></SearchBar> */}
       <FooterMain></FooterMain>
     </div>
   );
